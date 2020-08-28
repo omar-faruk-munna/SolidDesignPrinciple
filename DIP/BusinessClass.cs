38a31d5dba9f994267e3d02e1cdfe6083fc2c18e
hala <m@gmail.com>
2020-08-29 01:42:57 +0600

    dependency inversion principle

---
+++ DIP/BusinessClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIP
+{
+    public class BusinessClass : IRepo
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
---
+++ DIP/ControllerClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIP
+{
+    public class ControllerClass
+    {
+        private readonly IRepo repo;
+
+        public ControllerClass(IRepo repo)
+        {
+            this.repo = repo;
+        }
+
+        public void MyControllerMethod()
+        {
+            repo.MyMethod();
+        }
+
+    }
+}
