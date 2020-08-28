cc2e999611801808ca58b32c30226d7ee8edf698
hala <m@gmail.com>
2020-08-29 01:35:32 +0600

    Interface segregation

--- ISP/Program.cs
+++ ISP/Program.cs
@@ -9,4 +9,36 @@ namespace ISP
             Console.WriteLine("Hello World!");
         }
     }
+
+    //Fat Interface
+    public interface IFatUser
+    {
+        bool Register(string email, string username, string password);
+        bool Login(string username, string password);
+        void ResetPassword(string username);
+        void LogError(string error);
+        bool SendEmail(string emailContent);
+    }
+
+    //Interface Segregation
+    public interface IUser
+    {
+        bool Register(string email, string username, string password);
+        bool Login(string username, string password);
+        void ResetPassword(string username);
+    }
+
+    // Interface Segregation
+    public interface ILogger
+    {
+        void LogError(string error);
+    }
+
+    // Interface Segregation
+    public interface IEmail
+    {
+        bool SendEmail(string emailContent);
+    }
+
+
 }
