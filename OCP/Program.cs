aff5b009c55f648afd406b58eab6ec3ab3a18a5b
hala <m@gmail.com>
2020-08-29 01:46:49 +0600

    open close principle

--- OCP/Program.cs
+++ OCP/Program.cs
@@ -4,6 +4,17 @@ namespace OCP
 {
     class Program
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {MiddleName} {LastName}";
+        }
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
